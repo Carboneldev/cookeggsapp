c8c59a334edcda627576f5c5c6e364e2817ec17f
carboneldev <carbonelnata@gmail.com>
2024-04-14 15:37:23 +0200

    cook egg proj

---
+++ hard/hard.js
@@ -0,0 +1,45 @@
+const timer = 10;
+let amountTime = timer*60;
+function calculateTime(){
+    const countdown = document.querySelector("#countdown");
+    let minutes = Math.floor(amountTime/60);
+    let secondes = amountTime%60;
+    if (secondes<10){
+        secondes = "0" + secondes;
+    }
+    const myVideo = document.querySelector("#myVideo")
+countdown.textContent = `${minutes} : ${secondes}`;
+amountTime--;
+console.log(amountTime)
+if (amountTime<0){
+    stopTimer();
+    amountTime = 0;
+    changeVideo()
+}
+function stopTimer(){
+    clearInterval(timerId);
+}
+
+
+function changeVideo(){
+    myVideo.setAttribute("src", "chef_-_27678 (720p).mp4")
+}
+
+}
+
+
+    let timerId = setInterval(calculateTime, 1000);
+
+
+    const myButton = document.querySelector("#myButton");
+myButton.addEventListener('click',function(){
+    document.querySelector("#player").play();
+    document.querySelector("#myVideo").play();
+
+})
+
+const myButtonPause = document.querySelector("#myButtonPause");
+myButtonPause.addEventListener('click',function(){
+    document.querySelector("#player").pause();
+    document.querySelector("#myVideo").pause();
+        })
